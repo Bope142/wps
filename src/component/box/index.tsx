07dff13310c3266d0f8e2c0b01c074831b931dce
Norbert Yemuang <66695064+Bope142@users.noreply.github.com>
2024-04-25 10:20:45 +0100

    update src/component/box/index.tsx and style.scss

--- src/component/box/index.tsx
+++ src/component/box/index.tsx
@@ -1,5 +1,6 @@
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
 import "./style.scss";
+import { FaPlay } from "react-icons/fa";
 
 type Picture = {
   cover: string;
@@ -11,47 +12,43 @@ type PropsBoxCover = {
 };
 
 export const BoxCoverHouse = ({ pictureList }: PropsBoxCover) => {
-  const [indexCurrentCover, setIndexCurrentCover] = useState(0);
-  const [activeIndex, setActiveIndex] = useState<number>(0);
+  // const [indexCurrentCover, setIndexCurrentCover] = useState(0);
+  // // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  // const [, setActiveIndex] = useState<number>(0);
 
-  useEffect(() => {
-    const timer = setInterval(() => {
-      const nextIndex = (indexCurrentCover + 1) % pictureList.length;
-      setIndexCurrentCover(nextIndex);
-      setActiveIndex(nextIndex);
-    }, 8000);
+  // useEffect(() => {
+  //   const timer = setInterval(() => {
+  //     const nextIndex = (indexCurrentCover + 1) % pictureList.length;
+  //     setIndexCurrentCover(nextIndex);
+  //     setActiveIndex(nextIndex);
+  //   }, 8000);
 
-    return () => clearInterval(timer);
-  }, [indexCurrentCover, pictureList.length]);
-  const handleClick = (index: number) => {
-    setActiveIndex(index);
-    setIndexCurrentCover(index);
-  };
+  //   return () => clearInterval(timer);
+  // }, [indexCurrentCover, pictureList.length]);
+
   return (
-    <div
-      className="container__padding container__cover"
-      style={{
-        background: `url("${pictureList[indexCurrentCover].url}") center/cover fixed`,
-      }}
-    >
-      <div
-        className="others__cover"
-        style={{
-          gridTemplateColumns: `repeat(${pictureList.length},1fr)`,
-        }}
-      >
-        {pictureList &&
-          pictureList.map((picture, index) => (
-            <div
-              key={index}
-              className={`box__others ${
-                activeIndex === index ? "active__box" : ""
-              }`}
-              onClick={() => handleClick(index)}
-            >
-              <img src={picture.cover} alt={`Photo ${index + 1}`} />
-            </div>
-          ))}
+    <div className=" container__cover">
+      <div className="main__cover">
+        <div className="player__container">
+          <button className="btn__play">
+            <FaPlay />
+          </button>
+        </div>
+        <img src="/public/images/cover/cover10.webp" alt="" />
+      </div>
+      <div className="others__cover">
+        <div className="cover__box">
+          <img src="/public/images/cover/cover6.webp" alt="" />
+        </div>
+        <div className="cover__box">
+          <img src="/public/images/cover/cover4.webp" alt="" />
+        </div>
+        <div className="cover__box">
+          <img src="/public/images/cover/cover2.webp" alt="" />
+        </div>
+        <div className="cover__box">
+          <img src="/public/images/cover/cover3.webp" alt="" />
+        </div>
       </div>
     </div>
   );
